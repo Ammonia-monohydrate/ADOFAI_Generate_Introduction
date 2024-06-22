88eda5129a103f110a798bba5691189a790590d5
Ammonia-monohydrate <172064091@qq.com>
2024-06-22 23:26:59 +0800

    增加了异常处理.py

--- zhj.py
+++ zhj.py
@@ -18,6 +18,8 @@ print(f'''将按照以下格式输出：
 while True:
     file_path = input('输入q可以退出\n'  # 获取文件路径
                       '请拖入adofal文件：')
+    if len(file_path) >= 2 and file_path[0] == '"' and file_path[-1] == '"':
+        file_path = file_path[1:-1]
     if file_path == 'q' or file_path == 'Q':  # 如果输入是q或Q就退出
         break
     try:
@@ -25,9 +27,37 @@ while True:
     except FileNotFoundError:
         print('文件不存在！\n')
         continue
+    except PermissionError:
+        print('您可能没有权限打开该文件！\n')
+        continue
+    except OSError:
+        print('发生了一些不该发生的错误！\n'
+              '1：\n'
+              '您给出的文件路径中包含非法字符！\n'
+              '应该说，正常的文件路径不应该包含非法字符！\n'
+              '2：\n'
+              '文件已被占用！\n'
+              )
+        continue
+    except UnicodeDecodeError:
+        print('发生了一些不该发生的错误！\n'
+              '文件编码错误！\n'
+              '有可能，您给出的文件不是adofai文件\n'
+              )
+        continue
+    except Exception:
+        print('未知错误！\n')
+        continue
+
     file_contents = ''
     for x in range(10):  # 读取前10行，避免文件过大
-        file_contents += f.readline()
+        try:
+            file_contents += f.readline()
+        except UnicodeDecodeError:
+            print('在逐行读取时编码错误！\n'
+                  '有可能，您给出的文件不是adofai文件\n'
+                  )
+            continue
     try:  # 获取字段
         song_part = p_song_part.search(file_contents).group()
         artist_part = p_artist_part.search(file_contents).group()
